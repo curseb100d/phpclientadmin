84f8c575e2553cd37a81794a17dab5798e14899d
curseb100d <libyahan123@gmail.com>
2024-10-18 17:59:29 +0800

    Version 1.0.0

---
+++ components/user_header.php
@@ -0,0 +1,39 @@
+<?php
+if(isset($message)){
+    foreach($message as $message){
+        echo '
+        <div class = "message">
+            <span>'.$message.'</span>
+            <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+        </div>
+        ';
+    }
+}
+?>
+
+<header class="header">
+    <section class="flex">
+        <a href="home.php" class="logo">yum-yum 😋</a>
+
+        <nav class="navbar">
+            <a href="home.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="menu.php">Menu</a>
+            <a href="orders.php">Orders</a>
+            <a href="contact.php">Contact</a>
+        </nav>
+
+        <div class="icons">
+            <?php
+                $count_user_cart_items = $conn->prepare("SELECT * FROM `cart` WHERE user_id = ?");
+                $count_user_cart_items->execute([$user_id]);
+                $total_user_cart_items = $count_user_cart_items->rowCount();
+            ?>
+            <a href="search.php"><i class="fas fa-search"></i></a>
+            <a href="cart.php"><i class="fas fa-shopping-cart"></i><span>(<?= $total_user_cart_items ?>)</span></a>
+            <div id="user-btn" class="fas fa-user"></div>
+            <div id="menu-btn" class="fas fa-bars"></div>
+        </div>
+
+    </section>
+</header>
---
+++ home.php
@@ -0,0 +1,37 @@
+<?php
+include 'components/config.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+    $user_id = $_SESSION['user_id'];
+} else {
+    $user_id = '';
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Font Awesome CDN Link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+    
+    <!-- Custom CSS File Link -->
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Home</title>
+</head>
+<body>
+
+    <!-- Header Section Starts -->
+    <?php include 'components/user_header.php' ?>
+    <!-- Header Section Ends -->
+    
+    <!-- Custom JS File Link -->
+    <script src="script.js"></script>
+
+</body>
+</html>
