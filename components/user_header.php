1e84ec98f6df728af41824d53b604598cb916cb0
curseb100d <libyahan123@gmail.com>
2024-10-21 17:39:01 +0800

    Version 1.1.0

--- components/user_header.php
+++ components/user_header.php
@@ -35,5 +35,26 @@ if(isset($message)){
             <div id="menu-btn" class="fas fa-bars"></div>
         </div>
 
+        <div class="profile">
+            <?php
+                $select_profile = $conn->prepare("SELECT * FROM `users` WHERE id = ?");
+                $select_profile->execute([$user_id]);
+                if($select_profile->rowCount() > 0){
+                    $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+            ?>
+                <p class="name"><?= $fetch_profile['name']; ?></p>
+                <div class="flex">
+                    <a href="profile.php" class="btn">profile</a>
+                    <a href="components/user_logout.php" onclick="return confirm('logout from this website?');" class="delete-btn">logout</a>
+                </div>
+            <?php
+            }
+            else {
+            ?>
+                <p class="name"></p>
+            <?php
+            }
+            ?>
+        </div>
     </section>
 </header>
